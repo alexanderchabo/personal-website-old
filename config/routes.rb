b40c895d907746d10d7611da12963b5ebdb457cb
alexander.chabo <alexander.chabo@gmail.com>
2019-02-02 20:29:37 -0500

    delete everything

--- config/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  root :to => "home#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
--- db/migrate/20180603170931_add_company_to_experiences.rb
+++
@@ -1,5 +0,0 @@
-class AddCompanyToExperiences < ActiveRecord::Migration[5.2]
-  def change
-    add_column :experiences, :company, :string
-  end
-end
