7cfa1b6b81828f6c9920eb00eb849f56e70edc22
alexander.chabo <alexander.chabo@gmail.com>
2019-02-07 11:39:33 -0500

    test

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -24,13 +24,13 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    // {
-    //   resolve: `gatsby-source-filesystem`,
-    //   options: {
-    //     name: `images`,
-    //     path: `${__dirname}/src/assets/images`
-    //   }
-    // },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images/`
+      }
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     // {
