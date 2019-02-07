6f23994180ba2cd96a879dac8869fd4f232dac9e
alexander.chabo <alexander.chabo@gmail.com>
2019-02-07 11:42:16 -0500

    Commented out plugins

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -24,15 +24,15 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/assets/images/`
-      }
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `images`,
+    //     path: `${__dirname}/src/assets/images/`
+    //   }
+    // },
+    // `gatsby-transformer-sharp`,
+    // `gatsby-plugin-sharp`,
     // {
     //   resolve: `gatsby-plugin-manifest`,
     //   options: {
