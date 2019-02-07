0928ba12e4cdc543d43f7342330b89dcb6812b74
alexander.chabo <alexander.chabo@gmail.com>
2019-02-07 18:50:48 -0500

    Remark changes

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -28,27 +28,23 @@ module.exports = {
     `gatsby-plugin-sass`,
     `gatsby-v2-plugin-page-transitions`,
     `gatsby-plugin-netlify-cms`,
-    // {
-    //   resolve: `gatsby-source-filesystem`,
-    //   options: {
-    //     name: `images`,
-    //     path: `${__dirname}/src/assets/images/`
-    //   }
-    // },
-    // `gatsby-transformer-sharp`,
-    // `gatsby-plugin-sharp`,
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: `gatsby-starter-default`,
-    //     short_name: `starter`,
-    //     start_url: `/`,
-    //     background_color: `#663399`,
-    //     theme_color: `#663399`,
-    //     display: `minimal-ui`,
-    //     icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
-    //   }
-    // },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images/`
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`
+      }
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    'gatsby-transformer-remark',
     {
       resolve: `gatsby-plugin-typography`,
       options: {
--- gatsby/src/pages/work.tsx
+++ gatsby/src/pages/work.tsx
@@ -1,14 +1,15 @@
 import * as React from 'react';
+import { graphql } from 'gatsby';
 
 import Layout from '../components/Layout/Layout';
 import SEO from '../components/SEO/SEO';
-import Link from '../components/Link/Link';
 import Experiences from '../components/Experiences/Experiences';
 import Resume from '../data/resume';
 
-const Work = () => (
+const Work = ({ data }) => (
   <Layout>
     <SEO title='Work' />
+    {console.log(data)}
     <Experiences
       heading='Work'
       experiences={Resume.works.map(work => ({
@@ -20,4 +21,14 @@ const Work = () => (
   </Layout>
 );
 
+export const pageQuery = graphql`
+  query($path: String!) {
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      html
+      frontmatter {
+        position
+      }
+    }
+  }
+`;
 export default Work;
