e2c45adb18289d601125e23b6bf88ecdf4e6de91
Alexander Chabo <alexander.chabo@gmail.com>
2020-02-09 23:13:28 -0500

    Removed old stuff

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,18 +1,15 @@
-import * as React from 'react';
+import * as React from "react";
 
 // Components
-import Layout from 'components/Layout/Layout';
-import SEO from 'components/SEO/SEO';
-import HeroBanner from 'components/HeroBanner/HeroBanner';
-
-// Styles
-import * as styles from './404.module.scss';
+import Layout from "components/Layout/Layout";
+import SEO from "components/SEO/SEO";
+import HeroBanner from "components/HeroBanner/HeroBanner";
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title='404: Not found' />
+    <SEO title="404: Not found" />
     <HeroBanner
-      title='404'
+      title="404"
       subTitle="You just hit a route that doesn't exist... the sadness."
     />
   </Layout>
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,30 +1,27 @@
-import * as React from 'react';
-import { Fade } from 'react-reveal';
+import * as React from "react";
+import { Fade } from "react-reveal";
 
 // Components
-import Layout from 'components/Layout/Layout';
-import SEO from 'components/SEO/SEO';
-import HeroBanner from 'components/HeroBanner/HeroBanner';
-import PageContainer from 'components/PageContainer/PageContainer';
-
-// Styles
-import * as styles from './contact.module.scss';
+import Layout from "components/Layout/Layout";
+import SEO from "components/SEO/SEO";
+import HeroBanner from "components/HeroBanner/HeroBanner";
+import PageContainer from "components/PageContainer/PageContainer";
 
 const Contact = () => (
   <Layout>
-    <SEO title='Contact' />
-    <HeroBanner title='Say Hi!' subTitle='It never hurts to be friendly' />
+    <SEO title="Contact" />
+    <HeroBanner title="Say Hi!" subTitle="It never hurts to be friendly" />
     <PageContainer>
       <Fade bottom delay={500}>
         <p
           style={{
-            maxWidth: '500px',
-            margin: '1rem auto',
-            textAlign: 'center'
+            maxWidth: "500px",
+            margin: "1rem auto",
+            textAlign: "center"
           }}
         >
-          Get in touch with me by clicking{' '}
-          <a href='mailto:alexander.chabo@gmail.com'>here</a>
+          Get in touch with me by clicking{" "}
+          <a href="mailto:alexander.chabo@gmail.com">here</a>
         </p>
       </Fade>
     </PageContainer>
