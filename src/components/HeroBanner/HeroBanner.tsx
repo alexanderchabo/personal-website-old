358b6f131779295c8417f2b3bdcdb0fc2f19c5f5
Alexander Chabo <alexander.chabo@gmail.com>
2020-02-11 00:32:04 -0500

    Linting

--- src/components/HeroBanner/HeroBanner.tsx
+++ src/components/HeroBanner/HeroBanner.tsx
@@ -1,9 +1,9 @@
-import * as React from 'react';
-import { Fade } from 'react-reveal';
-import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
+import * as React from "react";
+import { Fade } from "react-reveal";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
 
-import * as styles from './HeroBanner.module.scss';
+import * as styles from "./HeroBanner.module.scss";
 
 interface HeroBannerProps {
   title: string;
@@ -15,11 +15,11 @@ interface HeroBannerProps {
 
 const useStyles = makeStyles(theme => ({
   button: {
-    marginTop: theme.spacing(4),
+    marginTop: theme.spacing(4)
   },
   input: {
-    display: 'none',
-  },
+    display: "none"
+  }
 }));
 
 const HeroBanner: React.FC<HeroBannerProps> = ({
@@ -35,16 +35,25 @@ const HeroBanner: React.FC<HeroBannerProps> = ({
     <div className={styles.heroBannerBody}>
       <h1>{title}</h1>
       {subTitle && <p>{subTitle}</p>}
-      {cta && ctaLink && <Button style={{
-        backgroundColor: "var(--bg",
-    }} href={ctaLink} variant="contained" className={classes.button}>{cta}</Button>}
+      {cta && ctaLink && (
+        <Button
+          style={{
+            backgroundColor: "var(--bg"
+          }}
+          href={ctaLink}
+          variant="contained"
+          className={classes.button}
+        >
+          {cta}
+        </Button>
+      )}
     </div>
   );
 
   return (
     <div className={styles.heroBanner}>
       <div className={styles.container}>
-        {disableFadeIn ? body : <Fade top>{body}</Fade>}
+        {disableFadeIn ? body : <Fade top={true}>{body}</Fade>}
       </div>
     </div>
   );
